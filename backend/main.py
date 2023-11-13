fb33f9db5c5c78fe0840a9f2d6ed4a162a895a35
Daniel Banariba <danielbanariba@protonmail.com>
2023-11-12 22:54:06 -0600

    Se le agrego el gitignore

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,9 @@ app = FastAPI()
 # Routers
 app.include_router(products.router)#que es el ".router"? es lo que hemos definido dentro del archivo router
 app.include_router(users.router)
-app.mount("/static", StaticFiles(directory="static"), name="static")#Al momento de poner http://127.0.0.1:8000/static y el nombre de la carpeta y el nombre del archivo tiene que verse o exponerse
+
+#Hecharle ojo a este pedazo de codigo
+#app.mount("/static", StaticFiles(directory="static"), name="static")#Al momento de poner http://127.0.0.1:8000/static y el nombre de la carpeta y el nombre del archivo tiene que verse o exponerse
 
 
 #url local: http://127.0.0.1:8000
